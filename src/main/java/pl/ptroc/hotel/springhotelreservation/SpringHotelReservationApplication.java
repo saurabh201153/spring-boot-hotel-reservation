ff59b590b43ea2141d534ad4acdd265e7f478c69
Pawel Troc <pawel.troc@execon.pl>
2018-01-06 11:59:16 +0100

    Added spring boot template appliation

---
+++ src/main/java/pl/ptroc/hotel/springhotelreservation/SpringHotelReservationApplication.java
@@ -0,0 +1,12 @@
+package pl.ptroc.hotel.springhotelreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHotelReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHotelReservationApplication.class, args);
+	}
+}
